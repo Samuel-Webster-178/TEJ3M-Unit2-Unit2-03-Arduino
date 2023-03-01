37e694d31ec5e3396c791b6be182d74d6911228b
Samuel-Webster-178 <99287154+Samuel-Webster-178@users.noreply.github.com>
2023-03-01 16:27:50 -0500

    Update blink.ino

--- blink.ino
+++ blink.ino
@@ -7,13 +7,13 @@
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(13, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(13, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
 }
